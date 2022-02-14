c275cf5204ed68c15d036390554372ef592dec0f
Kristoffer UiB konto <Kristoffer.Jensvoll-Johnsen@uib.no>
2022-02-14 14:01:29 +0100

    new title :)

--- src/main/java/core/DesktopLauncher.java
+++ src/main/java/core/DesktopLauncher.java
@@ -6,7 +6,7 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 public class DesktopLauncher {
     public static void main(String[] args) {
         Lwjgl3ApplicationConfiguration cfg = new Lwjgl3ApplicationConfiguration();
-        cfg.setTitle("hello-world");
+        cfg.setTitle("incredibly epic game");
         cfg.setIdleFPS(60);
         cfg.setWindowedMode(480, 320);
 
