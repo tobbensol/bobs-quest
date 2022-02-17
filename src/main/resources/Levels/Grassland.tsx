7b5627d743cd96c215d88a311710ff261f62d8c6
Martin Øvsttun <kuc015@student.uib.no>
2022-02-17 12:08:17 +0100

    Deleted duplicate level files

--- src/main/resources/Levels/Grassland.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.1" name="GrassLand" tilewidth="16" tileheight="16" tilecount="112" columns="16">
- <image source="Grassland_Terrain_Tileset.png" width="256" height="112"/>
-</tileset>
