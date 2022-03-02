fd7c2414f92de7fe92e0c960a441eba1cc4332fd
Espen.Kalvatn <Espen.Kalvatn@student.uib.no>
2022-03-02 21:08:44 +0100

    Wrote simple documentation for the interfaces: Moveable and Jumpable.

--- src/main/java/model/objects/Jumpable.java
+++ src/main/java/model/objects/Jumpable.java
@@ -1,10 +1,13 @@
 package model.objects;
 
+/**
+ * A jumpable object extends moveable and should be able to both move horizontally and vertically (jumping).
+ */
 public interface Jumpable extends Moveable {
 
     /**
-     * // TODO: Write documentation
-     * @param delta
+     * This method should make an object jump.
+     * @param delta - the time gap between previous and current frame
      */
     void jump(float delta);
 }
--- src/main/java/model/objects/Moveable.java
+++ src/main/java/model/objects/Moveable.java
@@ -1,11 +1,14 @@
 package model.objects;
 
+/**
+ * A moveable object should be able to move horizontally (to the left and to the right).
+ */
 public interface Moveable {
 
     /**
-     * // TODO: Write documentation
-     * @param delta
-     * @param isRight
+     * This method should move an object to either right or left.
+     * @param delta - the time gap between previous and current frame.
+     * @param isRight - boolean parameter to indicate whether you want to move to the right or not (to the left). true -> right, false -> left.
      */
     void moveHorizontally(float delta, boolean isRight);
 }
