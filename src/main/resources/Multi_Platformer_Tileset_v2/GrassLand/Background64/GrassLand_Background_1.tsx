58ca45f2d94608915c39cab81eb4be2e81aa6a24
Kristoffer UiB konto <Kristoffer.Jensvoll-Johnsen@uib.no>
2022-03-02 20:31:50 +0100

    Changes by Kristoffer: Made a new map and scaled up all the sprites in the Grasslands resource folder to fit 64x64 tiles

---
+++ src/main/resources/Multi_Platformer_Tileset_v2/GrassLand/Background64/GrassLand_Background_1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="GrassLand_Background_1" tilewidth="64" tileheight="64" tilecount="325" columns="25">
+ <image source="GrassLand_Background_1.png" width="1600" height="832"/>
+</tileset>
