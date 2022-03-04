c8ad5e5400d24d9c36649d83e5bd2a502727b74d
Martin Øvsttun <kuc015@student.uib.no>
2022-03-04 13:49:06 +0100

    Changes by Martin, Espen, Tobias, Kristoffer: Added getter for spawnPoints

--- src/main/java/model/helper/TiledMapHelper.java
+++ src/main/java/model/helper/TiledMapHelper.java
@@ -14,6 +14,7 @@ import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.Shape;
 import model.GameModel;
+import java.util.ArrayList;
 
 
 public class TiledMapHelper {
@@ -50,6 +51,10 @@ public class TiledMapHelper {
             float y = spawnPoint.getRectangle().getY();
             this.spawnPoints.add( new Vector2(x, y) );
         }
+    }
+
+    public ArrayList<Vector2> getSpawnPoints() {
+        return spawnPoints;
     }
 
 
