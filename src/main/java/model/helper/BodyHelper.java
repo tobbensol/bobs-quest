3b8f8689bcdcf0f2adae7e41c923430e55cc4592
Espen.Kalvatn <Espen.Kalvatn@student.uib.no>
2022-03-08 17:23:17 +0100

    Refactored a little in the bodyHelper class.

--- src/main/java/model/helper/BodyHelper.java
+++ src/main/java/model/helper/BodyHelper.java
@@ -6,53 +6,41 @@ import com.badlogic.gdx.physics.box2d.*;
 public class BodyHelper {
 
     public static Body BodyHelper(float x, float y, float width, float height, int density, World world, ContactType contactType) {
+
         BodyDef bodyDef = new BodyDef();
+        FixtureDef fixtureDef = new FixtureDef();
+        CircleShape circleShape = new CircleShape();
+
         bodyDef.type = BodyDef.BodyType.DynamicBody;
         bodyDef.position.set(x/Constants.PPM, y/Constants.PPM);
         bodyDef.fixedRotation = true;
+
         Body body = world.createBody(bodyDef);
 
-
-        // Creating the interacting shape (box or circle) around the object.
-        CircleShape circleShape = new CircleShape();
         circleShape.setRadius(width/2/Constants.PPM);
-        //PolygonShape shape = new PolygonShape();
-        //shape.setAsBox(width / 2 / Constants.PPM, height / 2 / Constants.PPM);
 
-        FixtureDef fixtureDef = new FixtureDef();
         fixtureDef.shape = circleShape;
         fixtureDef.density = density;
+        fixtureDef.filter.categoryBits = Constants.PLAYER_BIT;
+        fixtureDef.filter.maskBits = Constants.DEFAULT_BIT | Constants.COIN_BIT; // What an object can collide with.
         body.createFixture(fixtureDef).setUserData(contactType);
 
-        // Creating foot-sensor located at the bottom of the object.
-        PolygonShape polygonShape = new PolygonShape();
-        polygonShape.setAsBox((width/2) *0.6f / Constants.PPM, 2/Constants.PPM, new Vector2(0,-height/2/Constants.PPM), 0);
-        fixtureDef.shape = polygonShape;
-        fixtureDef.isSensor = true;
-        body.createFixture(fixtureDef).setUserData("foot");
-
-        // Creating head-sensor located at the top of the object.
-        polygonShape.setAsBox((width/2) * 0.4f / Constants.PPM, 2 / Constants.PPM, new Vector2(0, height/2/Constants.PPM), 0);
-        fixtureDef.shape = polygonShape;
-        fixtureDef.isSensor = true;
-        body.createFixture(fixtureDef).setUserData("head");
-
-        // Creating right side sensor
-        polygonShape.setAsBox(2 / Constants.PPM, (width/2)*0.9f / Constants.PPM, new Vector2(width/2/Constants.PPM, 0), 0);
-        fixtureDef.shape = polygonShape;
-        fixtureDef.isSensor = true;
-        body.createFixture(fixtureDef).setUserData("right");
-
-        // Creating left side sensor
-        polygonShape.setAsBox(2 / Constants.PPM, (width/2)*0.9f / Constants.PPM, new Vector2(-width/2/Constants.PPM, 0), 0);
-        fixtureDef.shape = polygonShape;
-        fixtureDef.isSensor = true;
-        body.createFixture(fixtureDef).setUserData("left");
-
-
-
         circleShape.dispose();
-        polygonShape.dispose();
+
+        createSensor("foot", fixtureDef,body,(width/2) *0.6f / Constants.PPM, 2/Constants.PPM, 0,-height/2/Constants.PPM);
+        createSensor("head", fixtureDef,body,(width/2) *0.4f / Constants.PPM, 2/Constants.PPM, 0,height/2/Constants.PPM);
+        createSensor("right", fixtureDef,body,2 / Constants.PPM, (width/2)*0.9f / Constants.PPM, width/2/Constants.PPM,0);
+        createSensor("left", fixtureDef,body,2 / Constants.PPM, (width/2)*0.9f / Constants.PPM, -width/2/Constants.PPM,0);
+
         return body;
+    }
+
+    private static void createSensor(String name, FixtureDef fixtureDef, Body body, float hx, float hy, float x, float y) {
+        PolygonShape shape = new PolygonShape();
+        shape.setAsBox(hx, hy, new Vector2(x,y), 0);
+        fixtureDef.shape = shape;
+        fixtureDef.isSensor = true;
+        body.createFixture(fixtureDef).setUserData(name);
+        shape.dispose();
     }
 }
