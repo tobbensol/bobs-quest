c34f03f6a3bcf3736f51c714a5ef1361f2c87af5
Espen.Kalvatn <Espen.Kalvatn@student.uib.no>
2022-03-15 13:44:49 +0100

    Changes by Tobias Soltvedt: Added new Goomba texture.

--- src/main/java/model/objects/Goomba.java
+++ src/main/java/model/objects/Goomba.java
@@ -18,7 +18,7 @@ public class Goomba extends MoveableObject {
         super(name, gameModel, x, y, density, ContactType.ENEMY, Constants.ENEMY_BIT, Constants.ENEMY_MASK_BITS);
         texturePath = "Multi_Platformer_Tileset_v2/Enemies/Goomba.png";
         texture = new Texture(texturePath);
-        textureRegion = new TextureRegion(getTexture(), 6*64, 0, 64, 64); // TODO: Proper texture
+        textureRegion = new TextureRegion(getTexture(), 64, 0, 64, 64); // TODO: Proper texture
 
         numMoves = 0;
     }
@@ -28,10 +28,12 @@ public class Goomba extends MoveableObject {
         super.update();
         if (numMoves == 0) {
             moveHorizontally(1.0f, false);
+            textureRegion.flip(true, false);
         }
         if (numMoves >= 120) {
             moveHorizontally(1.0f, true);
             numMoves = -120;
+            textureRegion.flip(true, false);
         }
         numMoves++;
     }
