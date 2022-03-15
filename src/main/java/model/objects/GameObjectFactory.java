efa96f4b3b1f26e5b55870388e9819f7024cf7fa
tobbensol <tobsol01@gmail.com>
2022-03-15 14:11:05 +0100

    Changed factory so that it doesn't specified density.

--- src/main/java/model/objects/GameObjectFactory.java
+++ src/main/java/model/objects/GameObjectFactory.java
@@ -13,9 +13,9 @@ public class GameObjectFactory {
     public GameObject create(String gameObject, float x, float y) {
         String objectString = gameObject.toUpperCase();
         return switch (objectString) {
-            case "COIN" -> new Coin(objectString, model, x, y, 1);
-            case "PLAYER" -> new Player(objectString, model, x, y, 0.8f);
-            case "GOOMBA" -> new Goomba(objectString, model, x, y, 1);
+            case "COIN" -> new Coin(objectString, model, x, y);
+            case "PLAYER" -> new Player(objectString, model, x, y);
+            case "GOOMBA" -> new Goomba(objectString, model, x, y);
             default -> null;
         };
     }
