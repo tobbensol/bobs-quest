881bbfdd1682dc2a6ae1d3f7f62ade99b3842263
Martin Øvsttun <kuc015@student.uib.no>
2022-03-15 09:53:58 +0100

    Tried removing contact on hit

--- src/main/java/model/objects/newCoin.java
+++ src/main/java/model/objects/newCoin.java
@@ -3,9 +3,13 @@ package model.objects;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import model.GameModel;
+import model.helper.BodyHelper;
+import model.helper.Constants;
 import model.helper.ContactType;
 
 public class newCoin extends StaticObject{
+
+    private boolean isDestroyed = false;
 
     public newCoin(String name, GameModel gameModel, float x, float y, float density, ContactType contactType) {
         super(name, gameModel, x, y, density, contactType);
@@ -20,5 +24,15 @@ public class newCoin extends StaticObject{
     @Override
     public void render(SpriteBatch batch) {
         batch.draw(texture, x-32, y-32, width, height);
+    }
+
+    public void onHit() { // TODO: Still registers contact
+        BodyHelper.setCategoryFilter(Constants.DESTROYED_BIT);
+        isDestroyed = true;
+//        removeCell();
+    }
+
+    public boolean isDestroyed() {
+        return isDestroyed;
     }
 }
