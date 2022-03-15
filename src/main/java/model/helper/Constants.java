612b254ae50616837d59d0150bfbe9d0721cdcde
Espen.Kalvatn <Espen.Kalvatn@student.uib.no>
2022-03-15 13:35:38 +0100

    Added mask_bits for generalizing BodyHelper

--- src/main/java/model/helper/Constants.java
+++ src/main/java/model/helper/Constants.java
@@ -10,5 +10,14 @@ public class Constants {
     public static final short DEFAULT_BIT = 1;
     public static final short PLAYER_BIT = 2;
     public static final short COIN_BIT = 4;
-    public static final short DESTROYED_BIT = 8;
+    public static final short ENEMY_BIT = 8;
+    public static final short DESTROYED_BIT = 16;
+
+    /**
+     * The bit values that a given body can interact with.
+     * TODO: These are not constants! Maybe in the wrong place????????
+     */
+    public static short PLAYER_MASK_BITS = DEFAULT_BIT | ENEMY_BIT | COIN_BIT;
+    public static short ENEMY_MASK_BITS = DEFAULT_BIT | PLAYER_BIT | ENEMY_BIT;
+    public static short COIN_MASK_BITS = PLAYER_BIT;
 }
