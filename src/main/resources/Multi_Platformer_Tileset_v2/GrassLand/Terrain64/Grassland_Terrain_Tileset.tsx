0391248939411f82e146a3dd7a5dc359405dddde
Kristoffer <kje022@uib.no>
2022-03-20 23:41:45 +0100

    Small tileset change

--- src/main/resources/Multi_Platformer_Tileset_v2/GrassLand/Terrain64/Grassland_Terrain_Tileset.tsx
+++ src/main/resources/Multi_Platformer_Tileset_v2/GrassLand/Terrain64/Grassland_Terrain_Tileset.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.2" name="Grassland_Terrain_Tileset64" tilewidth="64" tileheight="64" tilecount="112" columns="16">
- <image source="Grassland_Terrain_Tileset64.png" width="1024" height="448"/>
+<tileset version="1.8" tiledversion="1.8.2" name="Grassland_Terrain_Tileset" tilewidth="64" tileheight="64" tilecount="112" columns="16">
+ <image source="Grassland_Terrain_Tileset.png" width="1024" height="448"/>
 </tileset>
