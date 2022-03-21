ebe1198a093567d6a565815e2f4d9149f3e3c246
Kristoffer <kje022@uib.no>
2022-03-21 19:05:07 +0100

    Created method manageZoom() in GameCamera and added to update()

--- src/main/java/view/GameCamera.java
+++ src/main/java/view/GameCamera.java
@@ -5,6 +5,9 @@ import com.badlogic.gdx.math.Vector3;
 import model.GameModel;
 import model.objects.Player;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class GameCamera extends OrthographicCamera {
 
     GameModel gameModel;
@@ -13,8 +16,42 @@ public class GameCamera extends OrthographicCamera {
         this.gameModel = gameModel;
     }
 
+    public void update() {
 
-    public void update (){
+        manageZoom();
+        position.set( getAveragePlayerPosition() );
+        super.update();
+    }
+
+    private void manageZoom (){
+        ArrayList<Float> playerXs = new ArrayList<>();
+
+        for (Player player : gameModel.getPlayers()) {
+            if ( !player.isDead() ) {
+                playerXs.add( player.getPosition().x );
+            }
+        }
+
+        double paddedXWidth = viewportWidth * zoom * 0.68;
+
+        float minX = Collections.min( playerXs );
+        float maxX = Collections.max( playerXs );
+        float playersXDifference = maxX - minX;
+
+//        System.out.println("ScreenX: " + paddedXWidth + ", Player Difference: " + playersXDifference );
+
+        float minZoom = 1f;
+        float maxZoom = 1.45f;
+
+        if ( playersXDifference > paddedXWidth && zoom <= maxZoom ) {
+            zoom += 0.0016f;
+        }
+        if ( playersXDifference < paddedXWidth && zoom >= minZoom ) {
+            zoom -= 0.0016f;
+        }
+    }
+
+    private Vector3 getAveragePlayerPosition() {
         float averageX = 0;
         float averageY = 0;
         int playerCount = 0;
