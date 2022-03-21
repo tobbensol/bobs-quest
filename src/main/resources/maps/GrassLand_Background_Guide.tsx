ad2f6535479a0c540ea0b46ea067a0e5675db64f
Kristoffer <kje022@uib.no>
2022-03-21 19:00:57 +0100

    Added CameraTest.tmx and cleaned up map folder

--- src/main/resources/maps/GrassLand_Background_Guide.tsx
+++
@@ -1,5 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.2" name="GrassLand_Background_Guide" tilewidth="368" tileheight="208" tilecount="1" columns="1">
- <grid orientation="orthogonal" width="736" height="416"/>
- <image source="../Multi_Platformer_Tileset_v2/GrassLand/Background/GrassLand_Background_Guide.png" width="368" height="208"/>
-</tileset>
--- src/main/resources/maps/Grasslands.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.1" name="Grasslands" tilewidth="64" tileheight="64" tilecount="112" columns="16">
- <image source="Grassland_Terrain_Tileset64.png" width="1024" height="448"/>
-</tileset>
