13ccd4d98cf838a4841d767c38d509ede5b44fea
tobbensol <tobsol01@gmail.com>
2022-03-30 16:58:50 +0200

    experimental new camera position system that only accounts for the outermost players

--- src/main/java/model/GameModel.java
+++ src/main/java/model/GameModel.java
@@ -24,7 +24,7 @@ public class GameModel implements ControllableModel {
     private final GameController gameController;
     Level level;
     private boolean reload = false;
-    private int levelNR = 0;
+    private int levelNR = 4;
     private int numPlayers;
     private GameState state;
 
