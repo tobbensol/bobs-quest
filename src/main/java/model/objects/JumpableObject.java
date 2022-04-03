3418adafcca3ff9e1f01047f64334eaf299651ff
tobbensol <tobsol01@gmail.com>
2022-04-04 01:30:22 +0200

    player is grounded when they should be, and not grounded when they shouldnt be, i also made the foot sensor smaller

--- src/main/java/model/objects/JumpableObject.java
+++ src/main/java/model/objects/JumpableObject.java
@@ -5,6 +5,7 @@ import model.helper.ContactType;
 
 public abstract class JumpableObject extends MovableObject implements Jumpable {
 
+    private int groundedcount = 0;
     protected Boolean grounded;
 
     public JumpableObject(String name, Level level, float x, float y, float density, ContactType contactType, short categoryBits, short maskBits) {
@@ -12,8 +13,15 @@ public abstract class JumpableObject extends MovableObject implements Jumpable {
         grounded = false;
     }
 
+    //todo make other contacts work like this as well?
     public boolean setGrounded(boolean value) {
-        grounded = value;
+        if(value){
+            groundedcount++;
+        }
+        else{
+            groundedcount--;
+        }
+        grounded = groundedcount > 0;
         return grounded;
     }
 }
