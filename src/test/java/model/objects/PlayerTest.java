af93d3a3b80d96fdc3520dc5c11c48e0db1d870e
Martin Øvsttun <kuc015@student.uib.no>
2022-04-06 15:43:59 +0200

    Added tests for player movement and death

--- src/test/java/model/objects/PlayerTest.java
+++ src/test/java/model/objects/PlayerTest.java
@@ -1,12 +1,14 @@
 package model.objects;
 
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Filter;
+import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.utils.Array;
 import model.GameContactListener;
-import model.GameModel;
 import model.Level;
-import org.junit.jupiter.api.BeforeAll;
+import model.helper.Constants;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -17,11 +19,11 @@ import static org.mockito.Mockito.*;
 
 public class PlayerTest {
 
-    private static Player player;
-    private static World world;
+    private Player player;
+    private World world;
 
-    @BeforeAll
-    static void setup() {
+    @BeforeEach
+    void setup() {
         world = new World(new Vector2(0, 0), false);
         Level level = mock(Level.class);
         when(level.getWorld()).thenReturn(world);
@@ -35,14 +37,27 @@ public class PlayerTest {
     }
 
     @Test
-    void testPlayerMovesLeft() {
+    void testPlayerMovesHorizontally() {
         assertEquals(new Vector2(0, 0), player.getPosition());
         assertTrue(player.facingRight);
-        player.moveHorizontally(1.0f, false);
+
+        player.moveHorizontally(false);
+        player.update();
         doStep();
+
         assertTrue(player.getPosition().x < 0);
         assertEquals(0, player.getPosition().y);
         assertFalse(player.facingRight);
+
+        for (int i = 0; i < 3; i++) {
+            player.moveHorizontally(true);
+            player.update();
+            doStep();
+        }
+
+        assertTrue(player.getPosition().x > 0);
+        assertEquals(0, player.getPosition().y);
+        assertTrue(player.facingRight);
     }
 
     @Test
@@ -50,16 +65,59 @@ public class PlayerTest {
         assertEquals(new Vector2(0, 0), player.getPosition());
 
         player.setGrounded(true);
-        player.jump(1.0f);
+        player.jump();
+        player.update();
         doStep();
+        // Player's state gets updated the frame after the body gets updated
         player.update();
 
         assertTrue(player.getPosition().y > 0);
         assertEquals(0, player.getPosition().x);
-        assertEquals(Player.State.JUMPING, player.currentState);
-        assertEquals(Player.State.JUMPING, player.getState());
+        assertEquals(Player.State.JUMPING, player.getCurrentState());
         assertEquals(Player.State.STANDING, player.previousState);
         assertFalse(player.grounded);
+    }
+
+    @Test
+    void testPlayerDrops() {
+        assertEquals(new Vector2(0, 0), player.getPosition());
+
+        player.drop();
+        player.update();
+        doStep();
+        player.update();
+
+        assertTrue(player.getPosition().y < 0);
+        assertEquals(0, player.getPosition().x);
+        assertEquals(Player.State.FALLING, player.getCurrentState());
+        assertEquals(Player.State.STANDING, player.previousState);
+    }
+
+    @Test
+    void testPlayerCanGoThroughPlatform() {
+
+    }
+
+    @Test
+    void testPlayerDies() {
+        assertEquals(100, player.getHp());
+        player.takeDamage(90);
+        assertEquals(10, player.getHp());
+        player.takeDamage(10);
+        assertEquals(-1, player.getHp());
+        assertEquals(Player.State.STANDING, player.previousState);
+        assertEquals(Player.State.DEAD, player.currentState);
+
+        Array<Fixture> fixtureArray = player.body.getFixtureList();
+        for (int i = 0; i < fixtureArray.size; i++) {
+            Filter filter = fixtureArray.get(i).getFilterData();
+            assertEquals(Constants.DESTROYED_BIT, filter.categoryBits);
+            assertEquals(Constants.DESTROYED_MASK_BITS, filter.maskBits);
+        }
+        assertEquals(5, player.body.getLinearVelocity().y);
+
+        player.update();
+        assertEquals(Player.State.DEAD, player.getCurrentState());
     }
 
     private void doStep() {
