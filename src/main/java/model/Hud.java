cf109fe6322c85fddf8bc141da8e4a8a90201318
Espen.Kalvatn <Espen.Kalvatn@student.uib.no>
2022-04-06 13:15:54 +0200

    Added opacity filter when game is paused.

--- src/main/java/model/Hud.java
+++ src/main/java/model/Hud.java
@@ -1,10 +1,14 @@
 package model;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.utils.viewport.FitViewport;
@@ -19,35 +23,49 @@ public class Hud {
     private final Level level;
     private static Integer score;
 
-    private static Label scoreLabel;
-    private final Label levelLabel;
-    private final Label pausedLabel;
-    private final Label pauseInfoLabel;
+    private Label scoreLabel;
+    private Label levelLabel;
+    private Label pausedLabel;
+    private Label pauseInfoLabel;
 
     public Hud(SpriteBatch batch, Level level) {
         this.level = level;
         score = 0;
 
-
         viewport = new FitViewport(Boot.INSTANCE.getScreenWidth(), Boot.INSTANCE.getScreenHeight(), new OrthographicCamera());
         stage = new Stage(viewport, batch);
 
-        Table table = new Table();
-        table.top();
-        table.setFillParent(true);
+        Table levelInfoTable = createLevelInfoTab(level);
+        Table gamePausedTable = createPauseTab();
+        Image filter = createFilter();
 
-        scoreLabel = new Label(score + "/" + level.getGameObjects(Coin.class).size(), new Label.LabelStyle(new BitmapFont(), Color.WHITE));
-        levelLabel = new Label(level.toString(), new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+        stage.addActor(filter); // NB: filter must be added first!
+        stage.addActor(levelInfoTable);
+        stage.addActor(gamePausedTable);
+    }
 
-        scoreLabel.setFontScale(2f);
-        levelLabel.setFontScale(2f);
 
-        // First row:
-        table.add(levelLabel).expandX().padTop(10);
-        table.add(scoreLabel).expandX().padTop(10);
 
-        // Use table.row() for starting on a new line
+    public void update() {
+        score = level.getScore();
+        scoreLabel.setText(score + "/" + level.getGameObjects(Coin.class).size());
+        // TODO maybe not do this every frame
+        levelLabel.setText(level.toString());
+    }
 
+    public void pause() {
+        pausedLabel.setText("GAME PAUSED");
+        pauseInfoLabel.setText("Press P To Resume");
+        stage.getActors().get(0).getColor().a = 0.4f;
+    }
+
+    public void resume() {
+        pausedLabel.setText("");
+        pauseInfoLabel.setText("");
+        stage.getActors().get(0).getColor().a = 0f;
+    }
+
+    private Table createPauseTab() {
         Table gamePausedTable = new Table();
         gamePausedTable.center();
         gamePausedTable.setFillParent(true);
@@ -60,26 +78,36 @@ public class Hud {
         gamePausedTable.add(pausedLabel).expandX();
         gamePausedTable.row();
         gamePausedTable.add(pauseInfoLabel).expandX();
-
-        stage.addActor(table);
-        stage.addActor(gamePausedTable);
+        return gamePausedTable;
     }
 
-    public void update() {
-        score = level.getScore();
-        scoreLabel.setText(score + "/" + level.getGameObjects(Coin.class).size());
-        // TODO maybe not do this every frame
-        levelLabel.setText(level.toString());
+    private Table createLevelInfoTab(Level level) {
+        Table table = new Table();
+        table.top();
+        table.setFillParent(true);
 
+        scoreLabel = new Label(score + "/" + level.getGameObjects(Coin.class).size(), new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+        levelLabel = new Label(level.toString(), new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+
+        scoreLabel.setFontScale(2f);
+        levelLabel.setFontScale(2f);
+
+        table.add(levelLabel).expandX().padTop(10);
+        table.add(scoreLabel).expandX().padTop(10);
+
+        return table;
     }
 
-    public void pause() {
-        pausedLabel.setText("GAME PAUSED");
-        pauseInfoLabel.setText("Press P To Resume");
-    }
+    private Image createFilter() {
+        Pixmap pixmap = new Pixmap(1,1, Pixmap.Format.RGBA8888);
+        pixmap.setColor(Color.BLACK);
+        pixmap.fillRectangle(0, 0, 1, 1);
+        Texture texture = new Texture(pixmap);
+        pixmap.dispose();
 
-    public void resume() {
-        pausedLabel.setText("");
-        pauseInfoLabel.setText("");
+        Image filter = new Image(texture);
+        filter.setSize(Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
+        filter.getColor().a=0f;
+        return filter;
     }
 }
