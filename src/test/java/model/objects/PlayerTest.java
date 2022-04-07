8cf519f93aaa12cae119c23df5f7e0584e1c4073
Martin Øvsttun <kuc015@student.uib.no>
2022-04-07 13:26:26 +0200

    Made changes to testPlayerMovesHorizontally and testPlayerJumps to get more line coverage in Player

--- src/test/java/model/objects/PlayerTest.java
+++ src/test/java/model/objects/PlayerTest.java
@@ -30,7 +30,6 @@ public class PlayerTest {
         when(level.getWorld()).thenReturn(world);
         player = new Player(level, 0, 0);
 
-        // TODO: Check if player is grounded
         List<Player> players = new ArrayList<>();
         players.add(player);
         when(level.getGameObjects(Player.class)).thenReturn(players);
@@ -41,14 +40,19 @@ public class PlayerTest {
     void testPlayerMovesHorizontally() {
         assertEquals(new Vector2(0, 0), player.getPosition());
         assertTrue(player.facingRight);
+        assertFalse(player.grounded);
 
+        player.setGrounded(true);
         player.moveHorizontally(false);
         player.update();
         doStep();
+        player.update();
 
         assertTrue(player.getPosition().x < 0);
         assertEquals(0, player.getPosition().y);
         assertFalse(player.facingRight);
+        assertEquals(Player.State.STANDING, player.previousState);
+        assertEquals(Player.State.WALKING, player.getCurrentState());
 
         for (int i = 0; i < 3; i++) {
             player.moveHorizontally(true);
@@ -65,6 +69,8 @@ public class PlayerTest {
     void testPlayerJumps() {
         assertEquals(new Vector2(0, 0), player.getPosition());
 
+        player.setGrounded(true);
+        player.jump();
         player.setGrounded(true);
         player.jump();
         player.update();
@@ -107,26 +113,26 @@ public class PlayerTest {
         player.takeDamage(10);
         assertEquals(-1, player.getHp());
         assertEquals(Player.State.STANDING, player.previousState);
-        assertEquals(Player.State.DEAD, player.currentState);
+        assertTrue(player.isDead());
 
-        Array<Fixture> fixtureArray = player.body.getFixtureList();
+        Array<Fixture> fixtureArray = player.getBody().getFixtureList();
         for (int i = 0; i < fixtureArray.size; i++) {
             Filter filter = fixtureArray.get(i).getFilterData();
             assertEquals(Constants.DESTROYED_BIT, filter.categoryBits);
             assertEquals(Constants.DESTROYED_MASK_BITS, filter.maskBits);
         }
-        assertEquals(5, player.body.getLinearVelocity().y);
+        assertEquals(5, player.getBody().getLinearVelocity().y);
 
         player.update();
         assertEquals(Player.State.DEAD, player.getCurrentState());
     }
 
     @Test
-    void testTeleport() {
+    void testPlayerTeleport() {
         assertEquals(player.getPosition(), new Vector2(0, 0));
         player.setPosition(10, 10);
         assertEquals(player.getPosition(), new Vector2(10 * Constants.PPM, 10 * Constants.PPM));
-        assertEquals(player.body.getAngle(), 0);
+        assertEquals(player.getBody().getAngle(), 0);
     }
 
     private void doStep() {
