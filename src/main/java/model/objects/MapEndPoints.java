99064634a0a733e053cbf48f504175e2a9a81630
tobbensol <tobsol01@gmail.com>
2022-04-11 23:36:30 +0200

    updated MapEndPoints to kinematic body

--- src/main/java/model/objects/MapEndPoints.java
+++ src/main/java/model/objects/MapEndPoints.java
@@ -1,5 +1,6 @@
 package model.objects;
 
+import com.badlogic.gdx.physics.box2d.BodyDef;
 import model.Level;
 import model.helper.Constants;
 import model.helper.ContactType;
@@ -8,7 +9,7 @@ public class MapEndPoints extends DynamicObject {
 
     public MapEndPoints(String name, Level level, float x, float y) {
         super(name + " " + (level.getGameObjects(MapEndPoints.class).size() + 1), level, 100, 10000, x, y, 99999999, ContactType.CAMERA_WALL, Constants.CAMERA_WALL_BIT, Constants.CAMERA_WALL_MASK_BITS, true);
-        body.setGravityScale(0);
+        body.setType(BodyDef.BodyType.KinematicBody);
     }
 
     public float getWidth() {
