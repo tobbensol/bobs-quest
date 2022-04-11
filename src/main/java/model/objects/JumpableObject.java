7ad61a9c89dcd15db6ebef2a505581df6a06a722
tobbensol <tobsol01@gmail.com>
2022-04-11 12:06:52 +0200

    cleaned up JumpableObject code

--- src/main/java/model/objects/JumpableObject.java
+++ src/main/java/model/objects/JumpableObject.java
@@ -7,22 +7,20 @@ public abstract class JumpableObject extends MovableObject implements Jumpable {
 
     protected int groundedcount = 0;
     protected boolean canJump = true;
-    protected boolean grounded;
+    protected boolean grounded = false;
 
     public JumpableObject(String name, Level level, float x, float y, float density, ContactType contactType, short categoryBits, short maskBits) {
         super(name, level, x, y, density, contactType, categoryBits, maskBits);
-        grounded = false;
     }
 
     //todo make other contacts work like this as well?
-    public boolean setGrounded(boolean value) {
+    public void setGrounded(boolean value) {
         if (value) {
             groundedcount++;
         } else {
             groundedcount--;
         }
-        grounded = groundedcount > 0 && canJump;
-        return grounded;
+        updateGrouned();
     }
 
     public void updateGrouned() {
