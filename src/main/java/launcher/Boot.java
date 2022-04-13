be5c35074b7f6635163f8310bff4d6b794dfc105
Martin Øvsttun <kuc015@student.uib.no>
2022-04-13 11:57:08 +0200

    Removed call to Game.setScreen() and changed to GameModel.changeScreen()

--- src/main/java/launcher/Boot.java
+++ src/main/java/launcher/Boot.java
@@ -20,8 +20,7 @@ public class Boot extends Game {
         this.screenWidth = Gdx.graphics.getWidth();
         this.screenHeight = Gdx.graphics.getHeight();
         this.gameModel = new GameModel();
-
-        setScreen(new StartScreen(gameModel));
+        this.gameModel.changeScreen();
     }
 
     //TODO Maybe these two methods shoud be in gameScreen? Spotbugs complains about us using "INSTANCE = this;" to write to a static field further up in the code.
