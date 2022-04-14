92b2e378f9316fd5369e970840d5308169f16442
Espen.Kalvatn <Espen.Kalvatn@student.uib.no>
2022-04-14 17:42:06 +0200

    Tried to add buttons to pause HUD (not working yet)

--- src/main/java/model/Hud.java
+++ src/main/java/model/Hud.java
@@ -7,10 +7,10 @@ import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Image;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import launcher.Boot;
@@ -26,12 +26,16 @@ public class Hud {
     private Label pausedLabel;
     private Label pauseInfoLabel;
 
+    TextButton settings;
+
     public Hud(SpriteBatch batch, Level level) {
         this.level = level;
         score = 0;
 
         viewport = new FitViewport(Boot.INSTANCE.getScreenWidth(), Boot.INSTANCE.getScreenHeight(), new OrthographicCamera());
         stage = new Stage(viewport, batch);
+//        Gdx.input.setInputProcessor(stage);
+
 
         Table levelInfoTable = createLevelInfoTab(level);
         Table gamePausedTable = createPauseTab();
@@ -40,23 +44,54 @@ public class Hud {
         stage.addActor(filter); // NB: filter must be added first!
         stage.addActor(levelInfoTable);
         stage.addActor(gamePausedTable);
+
+        Skin skin = new Skin(Gdx.files.internal("src/main/resources/ui/uiskin.json"));
+        settings = new TextButton("Settings", skin);
+
+        settings.setPosition(100,100);
+        stage.addActor(settings);
     }
 
     public void update() {
         score = level.getScore();
         scoreLabel.setText(score + "/" + level.getGameObjects(Coin.class).size());
+
+
+
+
+        settings.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                //TODO: Make settings screen
+                level.getModel().setCurrentState(GameState.SETTINGS);
+                level.getModel().changeScreen();
+            }
+        });
+
     }
 
     public void pause() {
         pausedLabel.setText("GAME PAUSED");
         pauseInfoLabel.setText("Press P To Resume");
         stage.getActors().get(0).getColor().a = 0.4f;
+
+        settings.setVisible(true);
+//        settings.setDisabled(false);
+        //Gdx.input.setInputProcessor(stage);
+
+//        table.row();
+//        table.add(settings);
+
+
     }
 
     public void resume() {
         pausedLabel.setText("");
         pauseInfoLabel.setText("");
         stage.getActors().get(0).getColor().a = 0f;
+        settings.setVisible(false);
+
+//        settings.setDisabled(true);
     }
 
     private Table createPauseTab() {
@@ -88,6 +123,8 @@ public class Hud {
 
         table.add(levelLabel).expandX().padTop(10);
         table.add(scoreLabel).expandX().padTop(10);
+
+
 
         return table;
     }
