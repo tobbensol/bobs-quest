10636294dc394e6370af7133e2fabaad1e543c59
Kristoffer <kje022@uib.no>
2022-04-17 00:08:33 +0200

    Added desert setup map

---
+++ src/main/resources/Multi_Platformer_Tileset_v2/Desert/Tilesets/Desert_Background_Tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="Desert_Background_Tileset" tilewidth="64" tileheight="64" tilecount="870" columns="30">
+ <image source="Desert_Background_Tileset.png" width="1920" height="1856"/>
+</tileset>
