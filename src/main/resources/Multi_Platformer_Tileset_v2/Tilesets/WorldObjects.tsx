5b5ebf21498ec057e5385629c802d72872088114
Kristoffer <kje022@uib.no>
2022-04-17 00:18:30 +0200

    Fixed and improved all maps due to tileset changes

--- src/main/resources/Multi_Platformer_Tileset_v2/Tilesets/WorldObjects.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.4" name="WorldObjects" tilewidth="64" tileheight="64" tilecount="25" columns="5">
- <image source="WorldObject_Tileset.png" width="320" height="320"/>
-</tileset>
