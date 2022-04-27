97a670280d71b93771d3152bb46a04a2c058f333
Espen.Kalvatn <Espen.Kalvatn@student.uib.no>
2022-04-27 21:05:06 +0200

    Screens now extends AbstractScreen class instead of implementing Screen directly. All screens now has the scrolling background with player animation.

--- src/main/java/view/MainMenuScreen.java
+++ src/main/java/view/MainMenuScreen.java
@@ -1,54 +1,16 @@
 package view;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.GL30;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
-import com.badlogic.gdx.utils.viewport.FitViewport;
-import com.badlogic.gdx.utils.viewport.Viewport;
 import launcher.Boot;
 import model.GameModel;
 import model.GameState;
-import model.helper.Constants;
-import java.util.Arrays;
-
-public class MainMenuScreen implements Screen {
-    private final Viewport viewport;
-    private final Stage stage;
-    private final GameModel gameModel;
-    private Skin skin;
-
-    private float backgroundX = 0;
-    private float scrollingVelocity = 2f;
-    private float stateTime;
-
-    Texture background;
-    Texture player;
-    Animation<TextureRegion> animation;
-    SpriteBatch batch = new SpriteBatch();
+public class MainMenuScreen extends AbstractScreen {
 
 
     public MainMenuScreen(GameModel gameModel) {
-        this.gameModel = gameModel;
-        viewport = new FitViewport(Boot.INSTANCE.getScreenWidth(), Boot.INSTANCE.getScreenHeight(), new OrthographicCamera());
-        stage = new Stage(viewport, new SpriteBatch());
-        Gdx.input.setInputProcessor(stage);
-        skin = new Skin(Gdx.files.internal("ui/uiskin.json"));
-
-        background = new Texture("images/main-menu-background.png");
-
-        player = new Texture("Multi_Platformer_Tileset_v2/Players/Adventurer_Sprite_Sheet.png");
-        TextureRegion[][] frames = TextureRegion.split(player, Constants.TILE_SIZE / 2, Constants.TILE_SIZE / 2);
-        animation = new Animation<>(0.166f, Arrays.copyOf(frames[1], 8));
-        stateTime = 0;
+        super(gameModel);
     }
 
     @Override
@@ -92,48 +54,8 @@ public class MainMenuScreen implements Screen {
 
     @Override
     public void render(float delta) {
-        gameModel.update();
-        Gdx.gl.glClearColor(0, 0, 0, 1);
-        Gdx.gl.glClear(GL30.GL_COLOR_BUFFER_BIT);
-        batch.begin();
-        batch.draw(background,backgroundX,-350);
-
-        batch.draw(animation.getKeyFrame(stateTime,true),200,157,120,120);
-
-        batch.end();
-        stage.draw();
-
-        backgroundX -= scrollingVelocity;
-        if (backgroundX < -background.getWidth() + Boot.INSTANCE.getScreenWidth()) {
-            backgroundX = 0;
-        }
-        stateTime = stateTime +Gdx.graphics.getDeltaTime();
+        super.render(delta);
+        this.renderBackground();
     }
 
-    @Override
-    public void resize(int width, int height) {
-
-    }
-
-    @Override
-    public void pause() {
-
-    }
-
-    @Override
-    public void resume() {
-
-    }
-
-    @Override
-    public void hide() {
-
-    }
-
-    @Override
-    public void dispose() {
-        stage.dispose();
-        background.dispose();
-        player.dispose();
-    }
 }
