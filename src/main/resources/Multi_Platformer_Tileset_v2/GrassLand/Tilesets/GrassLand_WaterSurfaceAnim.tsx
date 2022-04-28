aa3e1bcd46cd0d3a60a471fd45e3368bd8bea4f1
Kristoffer <kje022@uib.no>
2022-04-28 23:31:01 +0200

    Added last part of level 1 and details around the map

---
+++ src/main/resources/Multi_Platformer_Tileset_v2/GrassLand/Tilesets/GrassLand_WaterSurfaceAnim.tsx
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="GrassLand_WaterSurfaceAnim" tilewidth="64" tileheight="64" tilecount="10" columns="10">
+ <image source="../Details/Animated/GrassLand_WaterSurfaceAnim.png" width="640" height="64"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="250"/>
+   <frame tileid="1" duration="250"/>
+   <frame tileid="2" duration="250"/>
+   <frame tileid="3" duration="250"/>
+   <frame tileid="4" duration="250"/>
+   <frame tileid="5" duration="250"/>
+   <frame tileid="6" duration="250"/>
+   <frame tileid="7" duration="250"/>
+   <frame tileid="8" duration="250"/>
+   <frame tileid="9" duration="250"/>
+  </animation>
+ </tile>
+</tileset>
