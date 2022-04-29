34094d1a41f8d5019b91f1769c7b31830129c71b
Martin Øvsttun <kuc015@student.uib.no>
2022-04-29 14:07:00 +0200

    Removed unused resource files

--- src/main/resources/Multi_Platformer_Tileset_v2/WorldObjects/Goal.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.2" name="Goal" tilewidth="64" tileheight="64" tilecount="2" columns="1">
- <image source="Goal.png" width="64" height="128"/>
-</tileset>
